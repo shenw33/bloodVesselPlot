b16834985d805beddc610c4a1efaae8aaa1f0ff8
shenw33 <shw515@lehigh.edu>
2022-10-20 20:27:39 -0700

    new files

--- plotting.py
+++ plotting.py
@@ -5,18 +5,20 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter, NullFormatter
 import matplotlib.ticker as mticker
 
-df = pd.read_excel("Vessel_Diameter.xlsx")
 
-df.rename(columns = { 'Vessel Diameter/(µm)':'Vessel'}, inplace = True)
+task = "Vessel_effective_diameter"
+df = pd.read_excel("./data/" + task + ".xlsx")
+# df.rename(columns = { 'Vessel Diameter/(µm)':'Vessel'}, inplace = True)
 
 font = {'family' : 'Arial',
-        'size'   : 24}
+        'size'   : 34}
+
 plt.rc('axes', linewidth=2)
 plt.rc('font', **font)
 
-fig, ax = plt.subplots( figsize = (12, 12))
+fig, ax = plt.subplots( figsize = (8, 12))
 
-sns.boxplot(data = df, x = "HUVEC Concentration", y = "Vessel", ax = ax)
+sns.boxplot(data = df, x = "Group", y = "Effective Diameter/(um)", ax = ax, linewidth = 4)
 
 # ax = sns.stripplot(data = df, x = "HUVEC Concentration", y = "Vessel" )
 
@@ -25,26 +27,28 @@ sns.boxplot(data = df, x = "HUVEC Concentration", y = "Vessel", ax = ax)
 # ax.xaxis.get_major_formatter().set_scientific(True)
 # plt.ticklabel_format(style='sci')
 
-# Previous way
-f = mticker.ScalarFormatter(useOffset=False, useMathText=True)
-g = lambda x,pos : "${}$".format(f._formatSciNotation('%1.10e' % x))
-fmt = mticker.FuncFormatter(g)
+# # Previous way
+# f = mticker.ScalarFormatter(useOffset=False, useMathText=True)
+# g = lambda x,pos : "${}$".format(f._formatSciNotation('%1.10e' % x))
+# fmt = mticker.FuncFormatter(g)
 
-# fmt(val) gives the format of power of 10 where val is a float in e format
-labels = [format(fmt(float(label.get_text()))) for label in ax.get_xticklabels()]
-ax.set_xticklabels(labels)
+# # fmt(val) gives the format of power of 10 where val is a float in e format
+# labels = [format(fmt(float(label.get_text()))) for label in ax.get_xticklabels()]
+# ax.set_xticklabels(labels)
 
 
 
 
 # fmt_new = mticker.ScalarFormatter(useMathText=True)
 # ax.set_xticklabels.set_major_formatter(mticker.ScalarFormatter(useMathText=True))
-
-
-ax.set( ylabel = "Vessel Diameter $\mu m$" ) 
+ax.set_ylim([30, 180])
+ax.set(xlabel = "Group")
+ax.set(ylabel = "Effective Diameter $(\mu m)$" ) 
 
 # ax.set(xlabel="Fluorescence Intensity")
 fig.tight_layout() 
 plt.show()
-# fig.savefig("boxplot.svg", dpi = 200)
+fig.savefig("boxplot-new" + task + ".svg", dpi = 200)
+
+fig.savefig("boxplot-new" + task + "-transp.svg", dpi = 200, transparent = True)
 pass
